589c9e851d46a788ae1c59a5848e379c9ea9b35a
KeerthishankarPunchithaya <punchithayakeerthishankar@gmail.com>
2022-01-31 16:32:36 +0530

    create a Database table

--- sqlite.py
+++ sqlite.py
@@ -4,3 +4,19 @@ import sqlite3
 
 conn = sqlite3.connect('customer.db')
 
+# create a cursor
+c = conn.cursor()
+
+# create a table
+c.execute("""CREATE TABLE customers (
+    first_name text,
+    last_name text,
+    email text
+)
+""")
+
+# commit our command
+conn.commit()
+
+# close our connection
+conn.close()
