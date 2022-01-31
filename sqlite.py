122afdc324ccfbcfe2a0b03f98ba2080a7d7af68
KeerthishankarPunchithaya <punchithayakeerthishankar@gmail.com>
2022-01-31 18:32:26 +0530

    Where Clause

--- sqlite.py
+++ sqlite.py
@@ -8,22 +8,13 @@ conn = sqlite3.connect('customer.db')
 c = conn.cursor()
 
 # Query the database
-c.execute("SELECT rowid, * FROM customers")
-# print(c.fetchone())
-# print(c.fetchmany(3))
+# c.execute("SELECT * FROM customers WHERE last_name = 'Punchithaya'")
+c.execute("SELECT * FROM customers WHERE email LIKE '%gmail.com'")
 
 items = c.fetchall()
 # print(items)
 for item in items:
     print(item)
-
-# print("NAME " + "\t\tEMAIL")
-# print("----" + "\t\t-----")
-# for item in items:
-#     print(item[0] + " " + item[1] + "\t\t" + item[2])
-
-
-# print("Command executed successfully...")
 
 # commit our command
 conn.commit()
