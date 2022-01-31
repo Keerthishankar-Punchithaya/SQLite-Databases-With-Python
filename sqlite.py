0a80072239da3555b5f1f707703466b832ee0828
KeerthishankarPunchithaya <punchithayakeerthishankar@gmail.com>
2022-01-31 18:43:31 +0530

    Update Records

--- sqlite.py
+++ sqlite.py
@@ -7,12 +7,22 @@ conn = sqlite3.connect('customer.db')
 # create a cursor
 c = conn.cursor()
 
+# Update Records
+# c.execute("""UPDATE customers SET first_name = "FirstName"
+#              WHERE last_name = "lastname1"
+# """)
+c.execute("""UPDATE customers SET first_name = "FirstName2"
+             WHERE rowid = 4
+""")
+
+
+conn.commit()
+
 # Query the database
-# c.execute("SELECT * FROM customers WHERE last_name = 'Punchithaya'")
-c.execute("SELECT * FROM customers WHERE email LIKE '%gmail.com'")
+c.execute("SELECT rowid, * FROM customers")
 
 items = c.fetchall()
-# print(items)
+
 for item in items:
     print(item)
 
