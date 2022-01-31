c9326380a9102a623024210e6276967461aa0c72
KeerthishankarPunchithaya <punchithayakeerthishankar@gmail.com>
2022-01-31 17:58:17 +0530

    Query and Fetchall

--- sqlite.py
+++ sqlite.py
@@ -7,16 +7,15 @@ conn = sqlite3.connect('customer.db')
 # create a cursor
 c = conn.cursor()
 
-many_customer = [
-                    ('name1','lastname1','name1@gmail.com'),
-                    ('name2','lastname2','name2@gmail.com'), 
-                    ('name3','lastname3','name3@gmail.com')
-                ]
+# Query the database
+c.execute("SELECT * FROM customers")
+# c.fetchone()
+# c.fetchmany(3)
 
-# c.execute("INSERT INTO customers VALUES ('Name2', 'Lastname2', 'name2@gmail.com')")
-c.executemany("INSERT INTO customers VALUES (?,?,?)", many_customer)
+print(c.fetchall())
 
-print("Command executed successfully...")
+
+# print("Command executed successfully...")
 
 # commit our command
 conn.commit()
